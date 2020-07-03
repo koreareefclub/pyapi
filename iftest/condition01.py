34eb17395d3398248f7b07adba58eca93ef2b774
koreareefclub <ksmyung1@hanmail.net>
2020-07-02 21:02:40 -0400

    Looping techniques

---
+++ iftest/condition01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3`
+
+# create the string hostname
+hostname = input("What value should we set for hostname?")
+# test logic with the `if` statement
+# what to do if this statement is found to be true
+if hostname == "MTG" or hostname.lower() == 'mtg':
+    print("The hostname was found to be mtg")
+
+print("exiting the script")    
+    
+
---
+++ moveplease01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import shutil
+import os
+
+os.chdir('/home/student/pyapi/')
+xname = input('What is the new name for kerrigan.obj? ')
+
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
